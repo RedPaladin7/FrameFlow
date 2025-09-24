2c5681a1ad201111250e1767f4d11e85c3cae9bd
RedPaladin7 <ishans7325@gmail.com>
2025-09-24 23:11:07 +0530

    init

---
+++ src/app/(website)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const Layout = ({children}: Props) => {
+    return (
+        <div>Layout</div>
+    )
+}
+
+export default Layout
